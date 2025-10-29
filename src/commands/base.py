91a57907d0ae490ac9737d2e5c6cef97092e0878
Fraimy1 <a.baxtiozinn@gmail.com>
2025-10-29 14:05:00 +0300

    add type hints

--- src/commands/base.py
+++ src/commands/base.py
@@ -1,3 +1,6 @@
+from src.core.models import ParsedCommand
+from src.core.services import Context
+
 class Command:
-    def execute(self, cmd, ctx):
+    def execute(self, cmd: ParsedCommand, ctx: Context):
         raise NotImplementedError

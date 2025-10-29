5219bc837762e7f74f9a86f5a3f006fef4cc83c2
Fraimy1 <a.baxtiozinn@gmail.com>
2025-10-29 05:51:04 +0300

    update config

--- src/config.py
+++ src/config.py
@@ -7,5 +7,14 @@ LOGS_FILENAME = 'shell.log'
 LOGS_FILE = ROOT / "logs" / LOGS_FILENAME
 HISTORY_FILE = ROOT / ".history"
 
+USER_WELCOME_MESSAGE = ""
+USER_GOODBYE_MESSAGE = ""
+
+COMMANDS = {
+    'cp': {'flags': ['--recursive'], 'options': [], 'positionals': 2},
+    'mv': {'flags': [], 'options': [], 'positionals': 2},
+    'rm': {'flags': ['--recursive'], 'options': [], 'positionals': 1},
+    'ls': {'flags': ['-l', '--long'], 'options': [], 'positionals': 1},
+}
 if __name__ == "__main__":
     print(ROOT)

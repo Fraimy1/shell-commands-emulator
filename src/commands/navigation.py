712c702f192bd130bdb55cfbd32342da4ffd594b
Fraimy1 <a.baxtiozinn@gmail.com>
2025-10-29 08:52:44 +0300

    fix cd

--- src/commands/navigation.py
+++ src/commands/navigation.py
@@ -4,7 +4,13 @@ from src.commands.base import Command
 
 class Cd(Command):
     def execute(self, cmd, ctx):
-        target = Path(cmd.positionals[0]).expanduser().resolve()
+        raw_target = Path(cmd.positionals[0])
+
+        if raw_target.is_absolute():
+            target = raw_target.resolve()
+        else:
+            target = (ctx.cwd / raw_target).expanduser().resolve() 
+
         if target.is_dir():
             ctx.cwd = target
         else:

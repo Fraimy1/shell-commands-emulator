11f6907d8dd31b00f7e0dfee0673b47bd4134f03
Fraimy1 <a.baxtiozinn@gmail.com>
2025-10-29 07:29:32 +0300

    update commands to use ctx + add cd

--- src/commands/files.py
+++ src/commands/files.py
@@ -3,15 +3,12 @@ Docstring for commands.files
 ls, cat, cp, mv, rm - here
 """
 
-# import typer
 from pathlib import Path
 from src.config import ROOT, HOME_DIR
 from datetime import datetime
 from src.core.models import ParsedCommand
+from src.core.services import Context
 
-# app = typer.Typer(help="File management commands")
-
-# @app.command()
 def ls(directory = Path.cwd(),long: bool = False):
     path = Path(directory).resolve()
     for entry in path.iterdir():
@@ -22,10 +19,11 @@ def ls(directory = Path.cwd(),long: bool = False):
             print(entry.name)
     return True
 
-def call_ls(cmd: ParsedCommand):
-    directory = Path(cmd.positionals[0]) if cmd.positionals else Path.cwd()
+def call_ls(cmd: ParsedCommand, ctx: Context):
+    directory = Path(cmd.positionals[0]) if cmd.positionals else ctx.cwd
     long = ('-l' in cmd.flags) or ('--long' in cmd.flags)
     ls(directory, long)
+
 
 def cp():
     ...
@@ -42,4 +40,4 @@ def rm():
 
 
 if __name__ == "__main__":
-    ls(long=True)
+    ls(long=False)
--- src/commands/navigation.py
+++ src/commands/navigation.py
@@ -1,2 +1,9 @@
-def cd():
-    ...
+from pathlib import Path
+from src.core.errors import ExecutionError
+
+def cd(cmd, ctx):
+    target = Path(cmd.positionals[0]).expanduser().resolve()
+    if target.is_dir():
+        ctx.cwd = target
+    else:
+        raise ExecutionError(f"Not a directory: {target}")

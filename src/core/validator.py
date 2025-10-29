371a7f4b3a5793c60fcb0c49051098091dbac5a3
Fraimy1 <a.baxtiozinn@gmail.com>
2025-10-29 08:53:05 +0300

    fix error output

--- src/core/validator.py
+++ src/core/validator.py
@@ -17,6 +17,6 @@ class Validator:
         min_pos, max_pos = spec['min_pos'], spec['max_pos']
         n_pos = len(cmd.positionals)
         if not (min_pos <= n_pos <= max_pos):
-            raise ValidationError(f"{cmd.name} expects from {min_pos} to {max_pos}" 
-                                  "positional arguments, but {n_pos} were given")
+            raise ValidationError(f"{cmd.name} expects from {min_pos} to {max_pos} " 
+                                  f"positional arguments, but {n_pos} were given")
         

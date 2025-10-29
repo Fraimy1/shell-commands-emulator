df63aacdc6387cc9ad0cded9ab7864bd7f32051d
Fraimy1 <a.baxtiozinn@gmail.com>
2025-10-29 09:31:14 +0300

    add proper ls --long

--- src/commands/files.py
+++ src/commands/files.py
@@ -7,17 +7,33 @@ from pathlib import Path
 from src.config import ROOT, HOME_DIR
 from datetime import datetime
 from src.commands.base import Command
+from tabulate import tabulate
+import stat
 
 class Ls(Command):
     def execute(self, cmd, ctx):
         directory = Path(cmd.positionals[0]).resolve() if cmd.positionals else ctx.cwd
         long = ('-l' in cmd.flags) or ('--long' in cmd.flags)
+        
+        data = []
         for entry in directory.iterdir():
             if long:
                 info = entry.stat()
-                print(info)
-            else:        
+                mode = stat.filemode(info.st_mode)
+                size = info.st_size
+                last_modified = datetime.fromtimestamp(info.st_mtime)
+                file_created = datetime.fromtimestamp(info.st_birthtime)
+                is_dir = entry.is_dir()
+                links = info.st_nlink
+                name = entry.name + ' (DIR)' if is_dir else entry.name
+                data.append((name, mode, links, size, last_modified, file_created))
+            else:
                 print(entry.name)
+
+        if long: 
+            headers = ['Name', 'Mode', 'Links', 'Size', 
+                       'Modified', 'Created']
+            print(tabulate(data, headers=headers, tablefmt='plain'))
 
 class Cp(Command):
     def execute(self, cmd, ctx):
